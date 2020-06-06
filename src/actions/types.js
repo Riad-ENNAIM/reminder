b3d8e866857c4aef37a096cf111f993b1528e406
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-06-06 18:32:22 +0100

    Tech Reducers, Actions and Types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
+// Log Types
 export const GET_LOGS = 'GET_LOGS';
 export const ADD_LOG = 'ADD_LOG';
 export const DELETE_LOG = 'DELETE_LOG';
@@ -7,3 +8,9 @@ export const UPDATE_LOG = 'UPDATE_LOG';
 export const SET_LOADING = 'SET_LOADING';
 export const LOGS_ERROR = 'LOGS_ERROR';
 export const SEARCH_LOGS = 'SEARCH_LOGS';
+
+// Tech Types
+export const GET_TECHS = 'GET_TECHS';
+export const ADD_TECH = 'ADD_TECH';
+export const DELETE_TECH = 'DELETE_TECH';
+export const TECHS_ERROR = 'TECHS_ERROR';
