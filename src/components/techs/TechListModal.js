3b993b4efd57a5ff9fd321d5fb0e577dfa32f84a
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-06-06 18:38:43 +0100

    Get Techs

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,34 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import TechItem from './TechItem';
+import { getTechs } from '../../actions/techActions';
 
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const TechListModal = ({ tech: { techs, loading }, getTechs }) => {
   useEffect(() => {
     getTechs();
     // eslint-disable-next-line
   }, []);
-
-  const getTechs = async () => {
-    setLoading(true);
-    const res = await fetch('/techs');
-    const data = await res.json();
-
-    setTechs(data);
-    setLoading(false);
-  };
 
   return (
     <div id='tech-list-modal' className='modal'>
       <div className='modal-content'>
         <h4>Technician List</h4>
         <ul className='collection'>
-          { !loading && techs.map(tech => <TechItem tech={tech} key={tech.id} />) }
+          { !loading && techs !== null && techs.map(tech => <TechItem tech={tech} key={tech.id} />) }
         </ul>
       </div>
     </div>
   );
 };
 
-export default TechListModal;
+TechListModal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  tech: state.tech
+});
+
+export default connect(
+  mapStateToProps,
+  { getTechs }
+)(TechListModal);
