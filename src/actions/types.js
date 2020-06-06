69b723292221043601394e715952ea1a2969933d
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-06-06 12:24:19 +0100

    Logs Reducers, Actions and Types

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+export const GET_LOGS = 'GET_LOGS';
+export const ADD_LOG = 'ADD_LOG';
+export const DELETE_LOG = 'DELETE_LOG';
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const UPDATE_LOG = 'UPDATE_LOG';
+export const SET_LOADING = 'SET_LOADING';
+export const LOGS_ERROR = 'LOGS_ERROR';
+export const SEARCH_LOGS = 'SEARCH_LOGS';
